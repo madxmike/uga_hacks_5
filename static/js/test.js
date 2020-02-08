5c61622f228f77fca13cce98d26e528c055173d6
Lennon Scariano <lennon.scariano@uga.edu>
2020-02-07 23:11:27 -0500

    fixed file paths, and file locations now conform to folder layout

--- static/js/test.js
+++ static/js/test.js
@@ -3,3 +3,6 @@ let map = L.map('mapid').setView([51.505, -0.09], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
+var marker = L.marker([51.5, -0.09]).addTo(map);
