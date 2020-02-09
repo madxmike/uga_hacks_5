767a3cd4c688370aa75b7afc87ac54559a518ca8
Lennon Scariano <lennon.scariano@uga.edu>
2020-02-09 05:35:19 -0500

    a

--- static/js/query.js
+++ static/js/query.js
@@ -30,13 +30,11 @@ map.on('popupclose', function(e) {
 
 document.getElementById("map").onclick = function () {
     document.getElementById("mapid").style.visibility = "visible";
-    document.getElementById("mapid").classList.remove("w-0");
     document.getElementById("list").style.visibility = "hidden";
     
 };
 document.getElementById("grid").onclick = function () {
     document.getElementById("list").style.visibility = "visible";
-    document.getElementById("mapid").classList.add("w-0");
     document.getElementById("mapid").style.visibility = "hidden";
 };
 
