a7d87b9316c125db8ac341b9e582870aecb8f4df
Michael Epps <mep053@gmail.com>
2020-02-08 22:30:47 -0500

    Fix port again

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"os/signal"
 	"syscall"
@@ -8,9 +9,19 @@ import (
 
 func main() {
 
-	go StartServer("80")
+	go StartServer(GetPort())
 
 	c := make(chan os.Signal, 2)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
+}
+
+func GetPort() string {
+	var port = os.Getenv("PORT")
+	// Set a default port if there is nothing in the environment
+	if port == "" {
+		port = "3000"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+	return ":" + port
 }
